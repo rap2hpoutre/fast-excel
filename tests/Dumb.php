178318c4d94fc9e7d11425f11115fe6b1d4df4bb
rap2h <raphaelht@gmail.com>
2018-04-09 13:35:41 +0200

    test callback on import

---
+++ tests/Dumb.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Rap2hpoutre\FastExcel\Tests;
+
+
+class Dumb
+{
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+}
--- tests/FastExcelTest.php
+++ tests/FastExcelTest.php
@@ -82,4 +82,30 @@ class FastExcelTest extends TestCase
         $this->assertEquals($original_collection, (new FastExcel)->configureCsv(';')->import(__DIR__ . '/test3.csv'));
         unlink(__DIR__ . '/test3.csv');
     }
+
+    /**
+     * @throws \Box\Spout\Common\Exception\IOException
+     * @throws \Box\Spout\Common\Exception\UnsupportedTypeException
+     * @throws \Box\Spout\Reader\Exception\ReaderNotOpenedException
+     */
+    public function testExcelImportWithCallback()
+    {
+        $collection = (new FastExcel)->import(__DIR__ . '/test1.xlsx', function ($value) {
+            return [
+                'test' => $value['col1']
+            ];
+        });
+        $this->assertEquals(
+            collect([['test' => 'row1 col1'], ['test' => 'row2 col1'], ['test' => 'row3 col1']]),
+            $collection
+        );
+
+        $collection = (new FastExcel)->import(__DIR__ . '/test1.xlsx', function ($value) {
+            return new Dumb($value['col1']);
+        });
+        $this->assertEquals(
+            collect([new Dumb('row1 col1'), new Dumb('row2 col1'), new Dumb('row3 col1')]),
+            $collection
+        );
+    }
 }
