a01fbddf5338f51f62b8b31eb022ecb5b2a2e7d5
gocep <popovski_goce@yahoo.com>
2018-07-03 12:20:49 +0200

    Update Importable.php (#30)
    * Update Importable.php
    
    * Update Importable.php

--- src/Importable.php
+++ src/Importable.php
@@ -81,6 +81,8 @@ trait Importable
                 }
                 if ($count_header > $count_row = count($row)) {
                     $row = array_merge($row, array_fill(0, $count_header - $count_row, null));
+                } elseif ($count_header < $count_row = count($row)) {
+                    $row = array_slice($row, 0, $count_header);
                 }
                 $collection[] = $callback ? $callback(array_combine($headers, $row)) : array_combine($headers, $row);
             }
