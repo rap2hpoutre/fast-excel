db64b6395b05e3e62316de1e267f1687a85cd53c
Johan Rosenson <10432296+johanrosenson@users.noreply.github.com>
2019-12-04 03:48:23 +0700

    add configure callbacks for Spout reader and writer (#136)
    * add configure callbacks for Spout reader and writer
    
    * style fixes

--- src/FastExcel.php
+++ src/FastExcel.php
@@ -4,7 +4,9 @@ namespace Rap2hpoutre\FastExcel;
 
 use Box\Spout\Common\Type;
 use Box\Spout\Reader\CSV\Reader as CSVReader;
+use Box\Spout\Reader\ReaderInterface;
 use Box\Spout\Writer\CSV\Writer as CSVWriter;
+use Box\Spout\Writer\WriterInterface;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
@@ -35,6 +37,16 @@ class FastExcel
         'encoding'  => 'UTF-8',
         'bom'       => true,
     ];
+
+    /**
+     * @var callable
+     */
+    protected $reader_configurator = null;
+
+    /**
+     * @var callable
+     */
+    protected $writer_configurator = null;
 
     /**
      * FastExcel constructor.
@@ -115,6 +127,34 @@ class FastExcel
     }
 
     /**
+     * Configure the underlying Spout Reader using a callback.
+     *
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function configureReaderUsing(?callable $callback = null)
+    {
+        $this->reader_configurator = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Configure the underlying Spout Reader using a callback.
+     *
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function configureWriterUsing(?callable $callback = null)
+    {
+        $this->writer_configurator = $callback;
+
+        return $this;
+    }
+
+    /**
      * @param \Box\Spout\Reader\ReaderInterface|\Box\Spout\Writer\WriterInterface $reader_or_writer
      */
     protected function setOptions(&$reader_or_writer)
@@ -129,6 +169,16 @@ class FastExcel
             if ($reader_or_writer instanceof CSVWriter) {
                 $reader_or_writer->setShouldAddBOM($this->csv_configuration['bom']);
             }
+        }
+
+        if ($reader_or_writer instanceof ReaderInterface && is_callable($this->reader_configurator)) {
+            call_user_func(
+                $this->reader_configurator, $reader_or_writer
+            );
+        } elseif ($reader_or_writer instanceof WriterInterface && is_callable($this->writer_configurator)) {
+            call_user_func(
+                $this->writer_configurator, $reader_or_writer
+            );
         }
     }
 }
