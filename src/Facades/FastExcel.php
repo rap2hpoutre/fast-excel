14153fa378f058ade2bb1dc6270ca3918c2e44bc
imxd <jayie153@gmail.com>
2020-10-27 13:32:42 +0800

    feat: Facades add document hint (#191)

--- src/Facades/FastExcel.php
+++ src/Facades/FastExcel.php
@@ -4,6 +4,19 @@ namespace Rap2hpoutre\FastExcel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class FastExcel.
+ *
+ * @method static \Rap2hpoutre\FastExcel\FastExcel data($data)
+ * @method static \Illuminate\Support\Collection import($path, callable $callback = null)
+ * @method static string export($path, callable $callback = null)
+ * @method static \Illuminate\Support\Collection importSheets($path, callable $callback = null)
+ * @method static \Rap2hpoutre\FastExcel\FastExcel configureCsv($delimiter = ',', $enclosure = '"', $eol = "\n", $encoding = 'UTF-8', $bom = false)
+ * @method static \Rap2hpoutre\FastExcel\FastExcel configureReaderUsing(?callable $callback = null)
+ * @method static \Rap2hpoutre\FastExcel\FastExcel configureWriterUsing(?callable $callback = null)
+ *
+ * @see \Rap2hpoutre\FastExcel\FastExcel
+ */
 class FastExcel extends Facade
 {
     /**
