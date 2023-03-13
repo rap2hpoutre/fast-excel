c6d2b8b707b85f7c43028d3b4cc969388d8b6c17
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2023-03-13 15:25:33 +0100

    Apply fixes from StyleCI (#320)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Facades/FastExcel.php
+++ src/Facades/FastExcel.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Facade;
  * Class FastExcel.
  *
  * @method static \Rap2hpoutre\FastExcel\FastExcel data($data)
- * @method static \Illuminate\Support\Collection import($path, callable $callback = null)
- * @method static string export($path, callable $callback = null)
- * @method static \Illuminate\Support\Collection importSheets($path, callable $callback = null)
+ * @method static \Illuminate\Support\Collection   import($path, callable $callback = null)
+ * @method static string                           export($path, callable $callback = null)
+ * @method static \Illuminate\Support\Collection   importSheets($path, callable $callback = null)
  * @method static \Rap2hpoutre\FastExcel\FastExcel configureCsv($delimiter = ',', $enclosure = '"', $encoding = 'UTF-8', $bom = false)
  * @method static \Rap2hpoutre\FastExcel\FastExcel configureReaderUsing(?callable $callback = null)
  * @method static \Rap2hpoutre\FastExcel\FastExcel configureWriterUsing(?callable $callback = null)
